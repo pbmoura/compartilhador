60f29897a5b2e1959fb331324dde9164627064b7
pbmoura <pbmoura@0076e1af-2e36-0410-a446-794e075828f2>
2007-08-20 22:00:26 +0000

    alterações de Lucas para o supernode fazer o papel de nameserver

--- trunk/kazou/src/node/INodeUI.java
+++ trunk/kazou/src/node/INodeUI.java
@@ -2,11 +2,12 @@ package node;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Vector;
 
 public interface INodeUI extends Remote {
 	
-	public void addMachines(Vector v) throws RemoteException;
+	public void addMachines(List v) throws RemoteException;
 	
 	public void finish() throws RemoteException;
 
--- trunk/kazou/src/supernode/ISuperNode.java
+++ trunk/kazou/src/supernode/ISuperNode.java
@@ -2,11 +2,13 @@ package supernode;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Vector;
 
 public interface ISuperNode extends Remote{
-	public void setNode(String NodeAddress, Vector filevector) throws RemoteException;
-	public Vector getVector(String file) throws RemoteException;
-	public Vector searchFile(String file, String name) throws RemoteException;
+	public void setNode(String NodeAddress, List filevector) throws RemoteException;
+	public List getFileNodes(String file) throws RemoteException;
+	public List searchFile(String file, String name) throws RemoteException;
 	public void disconnect(String name) throws RemoteException;
+	public void addSuperNode(String name) throws RemoteException;
 }
