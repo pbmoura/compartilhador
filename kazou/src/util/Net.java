aae27fb225349ea9e452b2752529f844a2df574b
yguarata <yguarata@0076e1af-2e36-0410-a446-794e075828f2>
2007-08-21 19:01:25 +0000

    classe para pegar ip local

---
+++ kazou/src/util/Net.java
@@ -0,0 +1,40 @@
+package util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class Net {
+
+	public static String getLocalIPAddress() throws IOException {
+		String ip = "";
+		
+		Enumeration<NetworkInterface> interfaces = 
+			NetworkInterface.getNetworkInterfaces();
+		
+		while (interfaces.hasMoreElements()) {
+			Enumeration<InetAddress> inetAddresses = 
+				interfaces.nextElement().getInetAddresses();
+			
+			while(inetAddresses.hasMoreElements()) {
+				InetAddress inet = inetAddresses.nextElement();
+				
+				if(!inet.isLoopbackAddress()) {
+					String hostAddress = inet.getHostAddress();
+					
+					if(hostAddress.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"))
+						System.out.println(hostAddress);
+				}
+			}
+		}
+		
+		return ip;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Net.getLocalIPAddress();
+	}
+	
+}
